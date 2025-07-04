1b3bb132784e355188a9cc330a6dbbb26bcdee71
orip <oriori1703@gmail.com>
2025-07-04 17:32:28 +0300

    Fix formatting

--- init.lua
+++ init.lua
@@ -585,9 +585,7 @@ require('lazy').setup({
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
           -- Toggle to show/hide diagnostic messages
-          map('<leader>td', function()
-            vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-          end, '[T]oggle [D]iagnostics')
+          map('<leader>td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, '[T]oggle [D]iagnostics')
 
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
